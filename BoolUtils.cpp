5562951568a1520be63f691f1dadb9ee3700dc45
Martin <84806126+MegapolisPlayer@users.noreply.github.com>
2022-10-21 16:42:10 +0200

    Create BoolUtils.cpp

---
+++ BoolUtils.cpp
@@ -0,0 +1,30 @@
+//CppUtils/BoolUtils - utility for booleans
+//CppUtils/BoolUtils.cpp © 2022 by MegapolisPlayer is licensed under CC BY 4.0 
+
+namespace CppUtils {
+	void Toggle(bool* aBool) {
+		if(*aBool == true) { *aBool = false; }
+		else { *aBool = true; }
+	}
+  enum struct BoolToTextMode {
+		LOWERCASE = 0, //all lowercase
+		FLCAPITAL,	   //first letter capital
+		CAPITAL		   //all capital
+	};
+	const char* BoolToText(bool aBool, BoolToTextMode aMode = BoolToTextMode::FLCAPITAL) {
+		if(aBool) {
+			switch(aMode) {
+				case(BoolToTextMode::LOWERCASE): return "true";
+				case(BoolToTextMode::FLCAPITAL): return "True";
+				case(BoolToTextMode::CAPITAL):	 return "TRUE";
+			}
+		}
+		else {
+			switch(aMode) {
+				case(BoolToTextMode::LOWERCASE): return "false";
+				case(BoolToTextMode::FLCAPITAL): return "False";
+				case(BoolToTextMode::CAPITAL):	 return "FALSE";
+			}
+		}
+	}
+}
