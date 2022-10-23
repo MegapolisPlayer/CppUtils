0cb8df946b9f4d22bc9b8dbcd37e016d2890f31a
Martin <84806126+MegapolisPlayer@users.noreply.github.com>
2022-10-23 20:42:38 +0200

    Create StringArrayCompare.cpp

---
+++ StringArrayCompare.cpp
@@ -0,0 +1,6 @@
+//CppUtils/StringArrayCompare - utility for C++ types and macros
+//CppUtils/StringArrayCompare.cpp © 2022 by MegapolisPlayer is licensed under CC BY 4.0 
+
+namespace CppUtils {
+
+}
