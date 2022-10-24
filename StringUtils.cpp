1d80f5f38f9fc3e09f4af973ecf57ec0298805e3
Martin <84806126+MegapolisPlayer@users.noreply.github.com>
2022-10-25 00:40:18 +0200

    Update StringUtils.cpp

--- StringUtils.cpp
+++ StringUtils.cpp
@@ -2,5 +2,24 @@
 //CppUtils/StringUtils.cpp © 2022 by MegapolisPlayer is licensed under CC BY 4.0 
 
 namespace CppUtils {
-
+	uint64_t StringLength(const char* const aString) {
+		uint64_t lId = 0;
+		while (aString[lId] != '\0') { lId++; }
+		return lId;
+	}	
+	bool StringCompare(const char* const aString1, const char* const aString2) {
+		uint64_t lString1Len = StringLength(aString1);
+		uint64_t lString2Len = StringLength(aString2);
+		if(lString1Len != lString2Len) { return false; } //string size not equal
+		for(uint64_t lId = 0; lId < lString1Len; lId++) {
+			if(aString1[lId] != aString2[lId]) { return false; }
+		}
+		return true;
+	}
+	bool IsStringInArray(const char** aArray, const uint64_t aArrayAmount, const char* aString) {
+		for(uint64_t lId = 0; lId < aArrayAmount; lId++) {
+			if(StringCompare(aArray[lId], aString)) { return true; }
+		}
+		return false;
+	}
 }
