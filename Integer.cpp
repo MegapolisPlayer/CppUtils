3fd680ea9c808d54481ad4e62137916414e60a38
Martin <84806126+MegapolisPlayer@users.noreply.github.com>
2022-10-30 14:10:42 +0100

    Update Integer.cpp

--- Integer.cpp
+++ Integer.cpp
@@ -2,11 +2,9 @@
 //CppUtils/Integer.cpp © 2022 by MegapolisPlayer is licensed under CC BY 4.0 
 
 namespace CppUtils {
-    bool IsBetween(const uint64_t aValue, const uint64_t aLower, const uint64_t aHigher) {
+	bool IsBetween(const uint64_t aValue, const uint64_t aLower, const uint64_t aHigher) {
 		return aValue >= aLower && aValue <= aHigher;
-    }
-    //for other utilities
-    #define CPP_UTILS_MINMAX 1
+	}
 	uint64_t Min(const uint64_t aNum1, const uint64_t aNum2) {
 		if(aNum1 < aNum2) { return aNum1; }
 		else { return aNum2; }
@@ -14,5 +12,5 @@ namespace CppUtils {
 	uint64_t Max(const uint64_t aNum1, const uint64_t aNum2) {
 		if(aNum1 > aNum2) { return aNum1; }
 		else { return aNum2; }
-  }
+	}
 }
