499ff3ac76a53091b3ae39e43d798cda52b5a9fb
Martin <84806126+MegapolisPlayer@users.noreply.github.com>
2022-10-31 20:23:34 +0100

    header comment fix

--- TextUtils.cpp
+++ TextUtils.cpp
@@ -1,5 +1,5 @@
-//CppUtils/StringUtils - utilities for strings
-//CppUtils/StringUtils.cpp © 2022 by MegapolisPlayer is licensed under CC BY 4.0 
+//CppUtils/TextUtils - utilities for chars and c-strings
+//CppUtils/TextUtils.cpp © 2022 by MegapolisPlayer is licensed under CC BY 4.0 
 
 #define CPP_UTILS_STRING
 
